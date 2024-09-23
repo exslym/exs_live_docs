5d4e6e225762de7c2d71573db6b56ef5a677520d
exslym <76844562+exslym@users.noreply.github.com>
2024-09-23 18:51:59 +0200

    typescript errors ignored

--- components/editor/plugins/FloatingToolbarPlugin.tsx
+++ components/editor/plugins/FloatingToolbarPlugin.tsx
@@ -235,6 +235,7 @@ export function createDOMRange(
 	try {
 		range.setStart(anchorDOM, anchorOffset);
 		range.setEnd(focusDOM, focusOffset);
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	} catch (e) {
 		return null;
 	}
